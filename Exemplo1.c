a94952bfbffdbb1933bf757d160d5381d0e821b2
m2ri16 <00000845849.marina@unicap.br>
2025-03-22 21:14:38 -0300

    Exemplo 1 update

--- Exemplo1.c
+++ Exemplo1.c
@@ -2,4 +2,6 @@ FILE *arquivo;
 arquivo = fopen("nomeDoArquivo.txt", "w");
 
 // manipule o arquivo
+fprintf(arquivo, "Programar é bacana!");
+fprintf(arquivo, "Escrevendo números: %d", 1234);
 fclose(arquivo);
