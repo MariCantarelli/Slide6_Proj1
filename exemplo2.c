d09a53f7a4374ff03a0a0a0b6102764f4beddc6f
m2ri16 <00000845849.marina@unicap.br>
2025-03-23 10:57:15 -0300

    Exemplo 2 update

--- exemplo2.c
+++ exemplo2.c
@@ -1,2 +1,9 @@
 fputc(caracter, arquivo); 
 
+FILE *arquivo;
+char texto[80] = "Isto é um teste!";
+int tamanho = strlen(texto);
+arquivo = fopen("nomeDoArquivo.txt", "w");
+for (int i = 0; i < tamanho; i++){
+fputc(texto[i], arquivo);
+}
